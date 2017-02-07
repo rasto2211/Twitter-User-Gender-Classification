96dbd73a50feea1e12e362dd9503cd1ca6bc9fab
Rastislav Rabatin <rastislav.rabatin@gmail.com>
2017-02-07 17:18:02 +0100

    Refactor some code from naive_bayes.py to common.py

--- common.py
+++ common.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 DATA_DIR = "data"
@@ -64,9 +65,51 @@ def load_data_split():
     return (train, test)
 
 
-def report_results(model, y, X, class_names, data_set_name):
+def print_results(y_true, y, X, data_set_name, class_names):
     print(data_set_name)
-    print(classification_report(y, model.predict(X), target_names=class_names))
-    print("Accuracy: {}".format(accuracy_score(y, model.predict(X))))
+    print(classification_report(y, y_true, target_names=class_names))
+    print("Accuracy: {}".format(accuracy_score(y, y_true)))
     print("==================================================================")
     print()
+
+
+def report_results(grid_search, y_train, X_train, y_test, X_test, class_names):
+    print("Best params: ", grid_search.best_params_)
+    print_results(grid_search.predict(X_train), y_train, X_train, "Train", class_names)
+    print_results(grid_search.predict(X_test), y_test, X_test, "Train", class_names)
+
+
+def compute_text_feats(vectorizer, rows, df):
+    return vectorizer.transform(df.ix[rows, "text_norm"])
+
+
+def compute_text_desc_feats(vectorizer, rows, df):
+    train_text = df.ix[rows, :]["text_norm"]
+    train_desc = df.ix[rows, :]["description_norm"]
+
+    return vectorizer.transform(train_text.str.cat(train_desc, sep=' '))
+
+
+def extract_feats_from_text(df, train_rows, test_rows):
+    df["text_norm"] = [normalize_text(text) for text in df["text"]]
+    df["description_norm"] = [normalize_text(text) for text in df["description"].fillna("")]
+
+    vectorizer = CountVectorizer()
+    vectorizer = vectorizer.fit(df.ix[train_rows, :]["text_norm"])
+
+    X_train = compute_text_feats(vectorizer, train_rows, df)
+    X_test = compute_text_feats(vectorizer, test_rows, df)
+
+    return (X_train, X_test)
+
+
+def extract_feats_from_text_and_desc(df, train_rows, test_rows):
+    vectorizer = CountVectorizer()
+    train_text = df.ix[train_rows, :]["text_norm"]
+    train_desc = df.ix[train_rows, :]["description_norm"]
+    vectorizer = vectorizer.fit(train_text.str.cat(train_desc, sep=' '))
+
+    X_train = compute_text_desc_feats(vectorizer, train_rows, df)
+    X_test = compute_text_desc_feats(vectorizer, test_rows, df)
+
+    return (X_train, X_test)
--- naive_bayes.py
+++ naive_bayes.py
@@ -1,27 +1,16 @@
 from common import load_data
 from common import load_data_split
 from common import encode_class_labels
-from common import normalize_text
 from common import report_results
+from common import extract_feats_from_text
+from common import extract_feats_from_text_and_desc
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import CountVectorizer
 
 
 JOBS = 4
 PARAMS = [{'alpha': [8, 4, 2, 1, 0.5, 0.25, 0.1, 0.07, 0.05, 0.03, 0.01, 0.001]}]
-
-
-def compute_text_feats(vectorizer, rows, df):
-    return vectorizer.transform(df.ix[rows, "text_norm"])
-
-
-def compute_text_desc_feats(vectorizer, rows, df):
-    train_text = df.ix[rows, :]["text_norm"]
-    train_desc = df.ix[rows, :]["description_norm"]
-
-    return vectorizer.transform(train_text.str.cat(train_desc, sep=' '))
 
 
 df = load_data()
@@ -30,39 +19,20 @@ train_rows, test_rows = load_data_split()
 y_train, y_test, class_names = \
     encode_class_labels(train_rows, test_rows, df)
 
-df["text_norm"] = [normalize_text(text) for text in df["text"]]
-df["description_norm"] = [normalize_text(text) for text in df["description"].fillna("")]
-
 print("Features only from Text")
 
-vectorizer = CountVectorizer()
-vectorizer = vectorizer.fit(df.ix[train_rows, :]["text_norm"])
-
-X_train = compute_text_feats(vectorizer, train_rows, df)
-X_test = compute_text_feats(vectorizer, test_rows, df)
+X_train, X_test = extract_feats_from_text(df, train_rows, test_rows)
 
 grid_search = GridSearchCV(MultinomialNB(), PARAMS, n_jobs=JOBS, verbose=5, cv=4,
                            scoring="f1")
 grid_search.fit(X_train, y_train)
-
-print("Best params: ", grid_search.best_params_)
-report_results(grid_search, y_train, X_train, class_names, "Train")
-report_results(grid_search, y_test, X_test, class_names, "Test")
+report_results(grid_search, y_train, X_train, y_test, X_test, class_names)
 
 print("Features from tweet text and description")
 
-vectorizer = CountVectorizer()
-train_text = df.ix[train_rows, :]["text_norm"]
-train_desc = df.ix[train_rows, :]["description_norm"]
-vectorizer = vectorizer.fit(train_text.str.cat(train_desc, sep=' '))
-
-X_train = compute_text_desc_feats(vectorizer, train_rows, df)
-X_test = compute_text_desc_feats(vectorizer, test_rows, df)
+X_train, X_test = extract_feats_from_text_and_desc(df, train_rows, test_rows)
 
 grid_search = GridSearchCV(MultinomialNB(), PARAMS, n_jobs=JOBS, verbose=5, cv=5,
                            scoring="f1")
 grid_search.fit(X_train, y_train)
-
-print("Best params: ", grid_search.best_params_)
-report_results(grid_search, y_train, X_train, class_names, "Train")
-report_results(grid_search, y_test, X_test, class_names, "Test")
+report_results(grid_search, y_train, X_train, y_test, X_test, class_names)
